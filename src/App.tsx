8262cd4869b5042450d4305d87bc309180420433
Alberto Lima <alberto.btlima@outlook.com>
2026-03-12 16:00:52 -0300

    adicionado componente Logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
+import { Container } from "./components/Container";
+import { Logo } from "./components/Logo";
+import { Heading } from "./components/Heading";
+
 import "./styles/theme.css";
 import "./styles/global.css";
-import { Container } from "./components/Container";
 
 export function App() {
   return (
     <>
       <Container>
-        <section>LOGO</section>
+        <Logo />
       </Container>
       <Container>
-        <section>MENU</section>
-      </Container>
-      <Container>
-        <section>TIMER</section>
+        <Heading>MENU</Heading>
       </Container>
     </>
   );
