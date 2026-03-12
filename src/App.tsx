aa1964aa91c3cc268d41966c10e73b24a5d921a5
Alberto Lima <alberto.btlima@outlook.com>
2026-03-12 15:32:11 -0300

    adicionado componente Container e pastas dos componentes organizada

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
-import { Heading } from "./components/Heading";
-
 import "./styles/theme.css";
 import "./styles/global.css";
+import { Container } from "./components/Container";
 
 export function App() {
   return (
     <>
-      <Heading>Hello, Vite + React!</Heading>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, dicta
-        natus exercitationem vel nesciunt impedit autem at mollitia beatae
-        aliquam odit laboriosam aut asperiores sunt rerum magnam sit!
-        Voluptatem, officiis!
-      </p>
+      <Container>
+        <section>LOGO</section>
+      </Container>
+      <Container>
+        <section>MENU</section>
+      </Container>
+      <Container>
+        <section>TIMER</section>
+      </Container>
     </>
   );
 }
