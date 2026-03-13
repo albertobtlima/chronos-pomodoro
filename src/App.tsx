9da9c817a57f200a88754dcdbba698a9f693a0c7
Alberto Lima <alberto.btlima@outlook.com>
2026-03-13 15:14:22 -0300

    add componente DefaultButton

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import { Menu } from "./components/Menu";
 import { CountDown } from "./components/CountDown";
 import { DefaultInput } from "./components/DefaultInput";
 import { Cycles } from "./components/Cycles";
+import { DefaultButton } from "./components/DefaultButton";
+import { PlayCircleIcon } from "lucide-react";
 
 export function App() {
   return (
@@ -42,7 +44,7 @@ export function App() {
           </div>
 
           <div className="formRow">
-            <button>Enviar</button>
+            <DefaultButton icon={<PlayCircleIcon />} />
           </div>
         </form>
       </Container>
