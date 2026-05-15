88c85507108eac4114d04d29a0c18b5fbb499c19
Alberto Lima <alberto.btlima@outlook.com>
2026-05-15 20:24:35 -0300

    add: arquivos de favicon, audio e webmanifest

--- src/contexts/TaskContext/taskReducer.ts
+++ src/contexts/TaskContext/taskReducer.ts
@@ -38,6 +38,21 @@ export function taskReducer(
       };
     }
 
+    case TaskActionTypes.COMPLETE_TASK: {
+      return {
+        ...state,
+        activeTask: null,
+        secondsRemaining: 0,
+        formattedSecondsRemaining: "00:00",
+        tasks: state.tasks.map((task) => {
+          if (state.activeTask && state.activeTask.id === task.id) {
+            return { ...task, completeDate: Date.now() };
+          }
+          return task;
+        }),
+      };
+    }
+
     case TaskActionTypes.RESET_STATE: {
       return state;
     }
